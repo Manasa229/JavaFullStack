3b92776e8a2b29da17a6c7827bb40705611cfaaf
Manasa229 <manasa229@gmail.com>
2023-07-13 18:07:51 -0500

    First commit

---
+++ JavaFullStack/src/Hello.java
@@ -0,0 +1,28 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System is a class
+		//out is the subclass
+		//println is the method
+		System.out.println("Hello World");
+
+	}
+
+}
+
+
+/*
+ * Data:
+ * keep the data in memory, so we need space
+ * This space is nothing but the variable
+ * 
+ * Datatype: type of data that 
+ * will be stored in the variable
+ * 
+ * 
+ * 
+ * 
+ */
+
